a8e6286afb471adb2450ac056b528811acadd0b9
newtsarthur <76273537+newtsarthur@users.noreply.github.com>
2021-09-15 20:06:45 -0300

    Add files via upload

---
+++ js/formulário.js
@@ -0,0 +1,15 @@
+// Copyright 2021 - newtsarthur
+function capturar() {
+    document.getElementById("non").style.display = "flex";
+    document.getElementById('teste').innerHTML = 'Agradeço pela sua sugestão! Volte sempre!💜';
+    setTimeout(function(){
+        document.getElementById("jaja").reset();
+    }, 850);
+}
+document.getElementById("non").style.display = "none";
+function dhas() {
+    setTimeout(function(){
+        document.getElementById("teste").innerHTML = '';
+    }, 150);
+    document.getElementById("non").style.display = "none";
+}
